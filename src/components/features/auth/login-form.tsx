e5bb644e077c8c981a86f359aefa569cc5f2d9db
John Smera <johnsmera@live.com>
2025-05-01 21:10:43 -0300

    feat(LoginForm): add buttons

--- src/components/features/auth/login-form.tsx
+++ src/components/features/auth/login-form.tsx
@@ -1,5 +1,9 @@
+import { GoogleIcon } from "@/components/shared/icons/google-icon";
 import { InputLabel } from "@/components/shared/input-label";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 interface LoginFormProps {
 	className?: string;
@@ -16,6 +20,33 @@ export function LoginForm({ className }: LoginFormProps) {
 
 			<InputLabel label="Email" placeholder="e-mail@website.com" />
 			<InputLabel label="Senha" placeholder="min. 8 caracteres" />
+
+			<div className="flex justify-between">
+				<div className="flex items-center gap-2">
+					<Checkbox id="remember" />
+					<label
+						htmlFor="remember"
+						className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 pb-[1px]"
+					>
+						Lembrar
+					</label>
+				</div>
+
+				<Link
+					href="/forgot-password"
+					className="text-sm text-primary font-medium pb-[1px]"
+				>
+					Esqueceu a senha?
+				</Link>
+			</div>
+
+			<div className="flex flex-col gap-4">
+				<Button>Entrar</Button>
+				<Button variant="outline">
+					<GoogleIcon />
+					Entrar com Google
+				</Button>
+			</div>
 		</div>
 	);
 }
