9982b4990124160fa50c99a4d2f559c999d5c803
John Smera <johnsmera@live.com>
2025-05-02 11:27:59 -0300

    refactor(DashPage): remove check session from component

--- src/app/dash/page.tsx
+++ src/app/dash/page.tsx
@@ -5,8 +5,6 @@ import { LogoutButton } from "@/components/features/auth/logout-button";
 export default async function DashPage() {
   const session = await auth();
 
-  if (!session?.user) redirect("/");
-
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
